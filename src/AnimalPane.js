649a08c847f13ad04ed8a4ea0bb6405aa1aa326a
gideon-steinberg <gideon_steinberg@hotmail.com>
2017-08-28 19:05:34 +1200

    :dog: look a dog

--- src/AnimalPane.js
+++ src/AnimalPane.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class AnimalPane extends Component {
+  getAnimal(){
+    return "http://i.imgur.com/gSEFJey.jpg";
+  }
   render(){
     return (
-      <div>
+      <div className="AnimalPane">
         <button className="FirstButton" > First Buttton </button>
 	<button className="SecondButton" > Second Button </button>
+	<img src={this.getAnimal()} className="TheAnimal" />
       </div>
     );
   }
