cbb54903a73f6f3c020ee1ba096905d7bb99a092
gideon-steinberg <gideon_steinberg@hotmail.com>
2017-08-28 18:57:26 +1200

    :smile: Two files connecting to each other

---
+++ src/AnimalPane.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AnimalPane extends Component {
+  render(){
+    return (
+      <div>
+        <button className="FirstButton" > First Buttton </button>
+	<button className="SecondButton" > Second Button </button>
+      </div>
+    );
+  }
+}
+
+export default AnimalPane;
